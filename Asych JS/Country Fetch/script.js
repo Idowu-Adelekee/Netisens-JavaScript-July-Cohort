5393fa3f9bf88b11965699f785680ba2a9548e62
Idowu Adeleke <work2idowu@gmail.com>
2025-10-03 13:06:37 +0100

    Async function lecture codes added

--- Asych JS/Country Fetch/script.js
+++ Asych JS/Country Fetch/script.js
@@ -1,7 +1,4 @@
 "use strict";
-
-console.log("check in");
-
 const countryContainer = document.querySelector(".countries");
 
 // https://restcountries.com/v2/name/${country}
@@ -14,7 +11,6 @@ const displayError = function (err) {
 };
 
 const displayCountry = function (data, neighbourClass) {
-  console.log(data);
   const countryhtml = ` 
   <article class="country ${neighbourClass}">
           <img class="country__img" src="${data.flag}" />
@@ -34,7 +30,6 @@ const displayCountry = function (data, neighbourClass) {
   countryContainer.insertAdjacentHTML("afterbegin", countryhtml);
 };
 
-// Comment start here
 // const getCountry = function (country) {
 //   const request = fetch(`https://restcountries.com/v2/name/${country}`)
 //     .then((response) => response.json())
@@ -45,6 +40,9 @@ const displayCountry = function (data, neighbourClass) {
 //     })
 //     .catch((error) => {
 //       displayError(error);
+//     })
+//     .finally(() => {
+//       countryContainer.style.opacity = 1;
 //     });
 // };
 
@@ -55,28 +53,76 @@ const displayCountry = function (data, neighbourClass) {
 // getCountry("ghana");
 // getCountry("portugal");
 
-/// Checking the country borders
+// // / Checking the country borders
 
-const getCountry = function (country) {
-  const request = fetch(`https://restcountries.com/v2/name/${country}`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data[0]);
+// const getCountry = function (country) {
+//   const request = fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data[0]);
 
-      displayCountry(data[0]);
-      const neighbour = data[0].borders[0];
+//       displayCountry(data[0]);
+//       const neighbour = data[0].borders[0];
 
-      console.log(neighbour);
+//       console.log(neighbour);
 
-      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
-    })
-    .then((respnse) => respnse.json())
-    .then((data) => {
-      displayCountry(data, "neighbour");
-    })
-    .catch((error) => {
-      displayError(error);
-    });
+//       return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+//     })
+//     .then((respnse) => respnse.json())
+//     .then((data) => {
+//       displayCountry(data, "neighbour");
+//     })
+//     .catch((error) => {
+//       displayError(error);
+//     });
+// };
+
+// getCountry("usa");
+
+// Consuming Promises with Async functions
+
+const getCountry = async function (country) {
+  try {
+    const request = await fetch(`https://restcountries.com/v2/name/${country}`);
+
+    const data = await request.json();
+    displayCountry(data[0]);
+
+    // Getting neighbouring country
+
+    const neighbour = data[0].borders[0];
+
+    const neighbourRequest = await fetch(
+      `https://restcountries.com/v2/alpha/${neighbour}`
+    );
+    const neighbourData = await neighbourRequest.json();
+    displayCountry(neighbourData, "neighbour");
+  } catch (err) {
+    // alert(`Something went wrong, wrong name ${err.message}`);
+    displayError(err);
+  }
 };
 
+// getCountry("nigeria");
 getCountry("usa");
+// getCountry("usa");
+// console.log("Hello");
+
+// const getUsersData = async function (user) {
+//   const request = await fetch(`https://dummyjson.com/users`);
+
+//   const response = await request.json();
+//   console.log(response);
+// };
+
+// getUsersData();
+
+// try {
+//   let age = 30;
+//   const name = "Wisdom";
+//   name = 31;
+// } catch (error) {
+//   alert(error.message);
+// }
+
+// Assignment, fetch data from dummy Jhttps://dummyjson.com/, using then method and using async function. Replace the fetch data with your html content.
